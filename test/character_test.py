9719831d8181c626ea4ce74fdb78dae51d7a8d83
amakineni <anil_makineni@mednax.com>
2022-03-10 20:31:24 +0530

    2nd passing test with iteration2

--- test/character_test.py
+++ test/character_test.py
@@ -1,8 +1,13 @@
 from rpg_combat.character import Character
 
-
 def test_character_starts_alive():
     character = Character()
     assert character.health == 1000
     assert character.level == 1
     assert character.alive
+
+def test_character_can_damage_character():
+    rogue = Character()
+    barbarian = Character()
+    barbarian.smack(rogue)
+    assert rogue.health < 990
